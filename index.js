feda9989ede11eb6b7f204eb9b837d410e99ea65
Elgrande Haytham <haythamhammioui16@gmail.com>
2025-06-23 21:38:36 +0100

    yopi

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
 const path = "./data.json";
-const date = moment().format();
 
-const data = {
-	date: date,
+const makeCommits = (n) => {
+	if (n === 0) return simpleGit().push();
+
+	const x = random.int(0, 54); // عدد الأسابيع
+	const y = random.int(0, 6);  // عدد الأيام
+	const date = moment()
+		.subtract(1, 'y')
+		.add(1, "d")
+		.add(x, "w")
+		.add(y, "d")
+		.format();
+
+	const data = { date };
+
+	console.log("Committing for date:", date);
+
+	jsonfile.writeFile(path, data, () => {
+		simpleGit()
+			.add([path])
+			.commit("commit", { '--date': date }, () => makeCommits(n - 1));
+	});
 };
 
-jsonfile.writeFile(path, data);
+makeCommits(10000);
