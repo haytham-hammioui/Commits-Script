d5028cda40a78fb041e519acd5aeff42d3900972
Elgrande Haytham <haythamhammioui16@gmail.com>
2025-07-05 12:02:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const path = "./data.json";
 const makeCommits = (n) => {
 	if (n === 0) return simpleGit().push();
 
-	const x = random.int(0, 54); // عدد الأسابيع
-	const y = random.int(0, 6);  // عدد الأيام
+	const x = random.int(0, 54); // weeks nb
+	const y = random.int(0, 6);  // days nb
 	const date = moment()
 		.subtract(1, 'y')
 		.add(1, "d")
